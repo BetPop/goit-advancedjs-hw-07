f895c4011e8a3afe528b692faf35c6fc8f539951
betpop <betpop.create@gmail.com>
2025-06-13 13:59:24 +0200

    add tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+  
+    constructor(protected key: Key) {}
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person entered the house.');
+      } else {
+        console.log('The door is closed. Person cannot enter.');
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('Door opened successfully.');
+      } else {
+        console.log('Wrong key!');
+      }
+    }
+  }
+  
+  // Тест
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());  // Відчиняє двері
+  house.comeIn(person);             // Заходить
+  
